d28f6d5ff756f6903ce15b066aa891fe77525df5
MohammadReza <mr.rahmatzade371@gmail.com>
2021-06-17 16:07:39 +0430

    comp lifecycle method

--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -8,6 +8,8 @@ class Menu extends Component {
         this.state = {  
             selectedDish: null          
         };
+
+        console.log('Menu Component Ctor Invoked.');
     }
 
     onDishSelect(dish){
@@ -31,19 +33,26 @@ class Menu extends Component {
         }
     }
 
+    componentDidMount(){
+        console.log('Menu Component did mount Invoked.');
+    }
+
     render() {
+        console.log('Menu Component Render Invoked.');
         const menu = this.props.dishes.map((dish) => {
+
             return (
                 <div key={dish.id} className="col-12 col-md-5 m-1">
                     <Card onClick={()=>{this.onDishSelect(dish)}}>                        
                         <CardImg width="100%" src={dish.image} alt={dish.name}/>                            
                         <CardImgOverlay>
-                            <CardTitle heading>{dish.name}</CardTitle>
+                            <CardTitle>{dish.name}</CardTitle>
                             <p>{dish.description}</p>
                         </CardImgOverlay>                                                                            
                     </Card>
                 </div>
             );
+            
         });
 
         return (
