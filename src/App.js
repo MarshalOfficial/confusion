0b431fab487aa4e061b74a689a4c51c771dbd2ea
MohammadReza <mr.rahmatzade371@gmail.com>
2021-06-25 16:29:39 +0430

    feat add react router and navbar

--- src/App.js
+++ src/App.js
@@ -1,29 +1,25 @@
-import { Navbar, NavbarBrand } from 'reactstrap';
-import Main from './components/MainComponent';
-import './App.css';
-import { DISHES } from './shared/dishes';
-import { Component } from 'react';
+import Main from "./components/MainComponent";
+import "./App.css";
+import { DISHES } from "./shared/dishes";
+import { Component } from "react";
+import { BrowserRouter } from "react-router-dom";
 
 class App extends Component {
-  constructor(props){
+  constructor(props) {
     super(props);
 
     this.state = {
-      dishes: DISHES
+      dishes: DISHES,
     };
   }
 
-
-  render(){
+  render() {
     return (
-      <div>
-        <Navbar dark color="primary"> 
-        <div className="container">
-          <NavbarBrand href="/">Fusion Restaurant</NavbarBrand>
-          </div>       
-        </Navbar>
-        <Main />
-      </div>
+      <BrowserRouter>
+        <div>
+          <Main />
+        </div>
+      </BrowserRouter>
     );
   }
 }
